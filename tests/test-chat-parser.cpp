f3bfbc6e0cf4d8115c76569904824712f53e333f
ochafik <ochafik@google.com>
2025-03-15 16:18:59 +0000

    refactor test-chat-parser

--- tests/test-chat-parser.cpp
+++ tests/test-chat-parser.cpp
@@ -91,56 +91,55 @@ static void test_regex() {
   }
 }
 
-static void test_json_with_dumped_args_no_args() {
-  auto test = [](const std::string & input, bool is_partial, const std::vector<std::vector<std::string>> & args_paths, const std::string & expected) {
-    common_chat_msg_parser builder(input, is_partial, {});
-    auto js = builder.try_consume_json_with_dumped_args(args_paths);
-    assert_equals(true, js.has_value());
-    assert_equals(is_partial, js->is_partial);
-    assert_equals(expected, args_paths.size() == 1 && args_paths[0].empty() ? js->value.get<std::string>() : js->value.dump());
-  };
+const std::vector<std::string> barely_healable_jsons = {
+  "{",
+  "{\"",
+  "{\"n",
+  "{\"name\"",
+  "{\"name\":",
+  "{\"name\":\"",
+  "{\"name\":\"python",
+};
 
+static void test(const std::string & input, bool is_partial, const std::vector<std::vector<std::string>> & args_paths, const std::string & expected) {
+  common_chat_msg_parser builder(input, is_partial, {});
+  auto js = builder.try_consume_json_with_dumped_args(args_paths);
+  assert_equals(true, js.has_value());
+  assert_equals(is_partial, js->is_partial);
+  assert_equals(expected, args_paths.size() == 1 && args_paths[0].empty() ? js->value.get<std::string>() : js->value.dump());
+}
+static void test_with_args(const std::string & input, const std::string & expected, bool parse_as_partial = true, bool is_partial = true) {
+  common_chat_msg_parser builder(input, parse_as_partial, {});
+  auto js = builder.try_consume_json_with_dumped_args({{"args"}});
+  assert_equals(true, js.has_value());
+  assert_equals(is_partial, js->is_partial);
+  assert_equals(expected, js->value.dump());
+}
+
+static void test_json_with_dumped_args_no_args() {
   // Normal JSON, nothing to heal, nothing to dump
   test("{\"name\": \"python\"}", false, {}, "{\"name\":\"python\"}");
   // Full json is args
   test("{\"name\": \"python\"}", false, {{}}, "{\"name\":\"python\"}");
 
-  {
-    std::vector<std::string> empty_srcs = {
-      "{",
-      "{\"",
-      "{\"n",
-      "{\"name\"",
-      "{\"name\":",
-      "{\"name\":\"",
-      "{\"name\":\"python",
-    };
-    // If the entire JSON is the arguments, healing it them dumping it produces the same output as the input (just reformatted).
-    test("{\"name\": \"python", true, {{}}, "{\"name\":\"python");
-    for (const auto & src : empty_srcs) {
-      test(src, true, {{}}, src);
-    }
-    // If the arguments are further down, don't heal partial content.
-    for (const auto & src : empty_srcs) {
-      test(src, true, {{"arguments"}}, "{}");
-    }
-    // But heal content that isn't partial.
-    test("{\"name\": \"python\"", true, {{"arguments"}}, "{\"name\":\"python\"}");
+  // If the arguments are further down, don't heal partial content.
+  for (const auto & src : barely_healable_jsons) {
+    test(src, true, {{"arguments"}}, "{}");
   }
+  // But heal content that isn't partial.
+  test("{\"name\": \"python\"", true, {{"arguments"}}, "{\"name\":\"python\"}");
 }
 
 static void test_json_with_dumped_args() {
-  auto test = [](const std::string & input, const std::string & expected, bool parse_as_partial = true, bool is_partial = true) {
-    common_chat_msg_parser builder(input, parse_as_partial, {});
-    auto js = builder.try_consume_json_with_dumped_args({{"args"}});
-    assert_equals(true, js.has_value());
-    assert_equals(is_partial, js->is_partial);
-    assert_equals(expected, js->value.dump());
-  };
+  // If the entire JSON is the arguments, healing it them dumping it produces the same output as the input (just reformatted).
+  test("{\"name\": \"python", true, {{}}, "{\"name\":\"python");
+  for (const auto & src : barely_healable_jsons) {
+    test(src, true, {{}}, src);
+  }
 
   // Full JSON w/ args
   for (auto parse_as_partial : {true, false}) {
-    test(
+    test_with_args(
       R"({"name": "python", "args": {"arg1": 1}})",
       R"({"name":"python","args":"{\"arg1\":1}"})",
       parse_as_partial,
@@ -149,77 +148,77 @@ static void test_json_with_dumped_args() {
   }
 
   // Partial JSON w/ partial args
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {")",
     R"({"foo":"bar","args":"{\""})"
   );
   // Partial args broken in object key
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"ar)",
     R"({"foo":"bar","args":"{\"ar"})"
   );
   // Partial args broken after object key
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1")",
     R"({"foo":"bar","args":"{\"arg1\""})"
   );
   // Partial args broken before object value
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1":)",
     R"({"foo":"bar","args":"{\"arg1\":"})"
   );
   // Partial args broken before object value (space)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1": )",
     R"({"foo":"bar","args":"{\"arg1\":"})"
   );
   // Partial args broken in object value that may not be complete (int)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1": 1)",
     R"({"foo":"bar","args":"{\"arg1\":"})"
   );
   // Partial args broken in object value that is complete (int)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1": 1 )",
     R"({"foo":"bar","args":"{\"arg1\":1"})"
   );
   // Partial args broken in object value that is incomplete (string)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1": ")",
     R"({"foo":"bar","args":"{\"arg1\":\""})"
   );
   // Partial args broken in object value that is complete (string)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1": "1")",
     R"({"foo":"bar","args":"{\"arg1\":\"1\""})"
   );
   // Partial args broken on array opening
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": [)",
     R"({"foo":"bar","args":"["})"
   );
   // Partial args broken on array value that is incomplete (int)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": [1)",
     R"({"foo":"bar","args":"["})"
   );
   // Partial args broken on array value that is complete (int)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": [1 )",
     R"({"foo":"bar","args":"[1"})"
   );
   // Partial args broken on array value that is complete (string)
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": ["1")",
     R"({"foo":"bar","args":"[\"1\""})"
   );
   // Partial args broken after array value
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": [1,)",
     R"({"foo":"bar","args":"[1,"})"
   );
   // Partial args broken on nested array
-  test(
+  test_with_args(
     R"({"foo": "bar", "args": {"arg1": [)",
     R"({"foo":"bar","args":"{\"arg1\":["})"
   );
